403e353806ced1524ebf96c1145e0819899fff23
Diogo Moura <Diogo Moura>
2016-03-02 01:13:52 +0000

    Json is now being read

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
 /**
  * Created by diogo on 02/03/2016.
  */
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.Iterator;
 
 public class Main {
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
 
+        try {
+
+            Object obj = parser.parse(new FileReader(
+                    "resources/cocelhosDataJson.json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            JSONArray concelhos = (JSONArray) jsonObject.get("concelhos");
+
+            // loop array
+            Iterator<JSONObject> iterator = concelhos.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
