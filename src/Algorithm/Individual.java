8e5ab477ae5a299e75fe8a5fed3393a4f292a1ed
Diogo Moura <Diogo Moura>
2016-04-13 20:05:34 +0100

    Function to calc distance between to coord

--- src/Algorithm/Individual.java
+++ src/Algorithm/Individual.java
@@ -79,4 +79,23 @@ public class Individual {
         return fitness;
     }
 
+    //TODO Criar uma classe para as funcoes de fitness
+    public static float distFrom(double lat1, double lng1, double lat2, double lng2) {
+        double earthRadius = 6371000; //meters
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLng = Math.toRadians(lng2 - lng1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        float dist = (float) (earthRadius * c);
+
+        return dist;
+    }
+
+    //para testes
+    public static void main(String[] args) {
+        System.out.println("Lisboa->Porto:" + Individual.distFrom(38.7222524, -9.1393366, 41.1579438, -8.629105299999999));
+    }
+
 }
